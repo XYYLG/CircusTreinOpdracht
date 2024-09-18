59115367710bc3dbe67cae09a4ac4299b693c46e
XYYLG <547214@student.fontys.nl>
2024-09-18 10:57:19 +0200

    Animal names changed.

--- CircusTreinOpdracht/Program.cs
+++ CircusTreinOpdracht/Program.cs
@@ -3,18 +3,18 @@ using CircusTreinOpdracht.Enums;
 
 List<Animal> AllAnimals = new List<Animal>()
 {
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Light, "Cat"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Light, "Koala"),
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Medium, "Dog"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Medium, "Deer"),
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Heavy, "Tiger"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Heavy, "Elephant"),
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Light, "Platypus"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Light, "Parrot"),
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Medium, "Penguin"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Medium, "Koala2"),
-    new Animal(AnimalTypes.Carnivore, AnimalSize.Heavy, "Lion"),
-    new Animal(AnimalTypes.Herbivore, AnimalSize.Heavy, "Hippopotamus")
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Light, "CarnivoreLight"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Light, "HerbivoreLight"),
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Medium, "CarnivoreMedium"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Medium, "HerbivoreMedium"),
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Heavy, "CarnivoreHeavy"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Heavy, "HerbivoreHeavy"),
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Light, "CarnivoreLight"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Light, "HerbivoreLight"),
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Medium, "CarnivoreMedium"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Medium, "HerbivoreMedium"),
+    new Animal(AnimalTypes.Carnivore, AnimalSize.Heavy, "CarnivoreHeavy"),
+    new Animal(AnimalTypes.Herbivore, AnimalSize.Heavy, "HerbivoreHeavy")
 };
 
  Train train = new Train();
