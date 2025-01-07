120d8af55f86afeffe367a3d63ce986f25ca3efd
XYYLG <547214@student.fontys.nl>
2025-01-07 11:28:36 +0100

    Removed unnecissarythings

--- CircusTreinOpdrachtTests/Classes/TrainTests.cs
+++ CircusTreinOpdrachtTests/Classes/TrainTests.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CircusTreinOpdracht.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CircusTreinOpdracht.Enums;
+﻿using CircusTreinOpdracht.Enums;
 
 namespace CircusTreinOpdracht.Classes.Tests
 {
